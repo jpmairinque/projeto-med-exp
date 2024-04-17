1fede93f36256bcef9729a3bf951d5a71dff7ac6
João Pedro Mairinque <joaopedro.mairinque@arkmeds.com>
2024-04-17 15:28:55 -0300

    remove if

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+student_files = [doc for doc in os.listdir()]
 student_notes = [open(_file, encoding='utf-8').read()
                  for _file in student_files]
 
