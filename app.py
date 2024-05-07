f86bf47698029b1aaf02df8f4bda12c61cab494b
João Pedro Mairinque <joaopedro.mairinque@arkmeds.com>
2024-05-07 17:55:00 -0300

    code to default

--- app.py
+++ app.py
@@ -18,7 +18,16 @@ plagiarism_results = set()
 
 def check_plagiarism():
     global s_vectors
-   
+    for student_a, text_vector_a in s_vectors:
+        new_vectors = s_vectors.copy()
+        a = 2
+        current_index = new_vectors.index((student_a, text_vector_a))
+        del new_vectors[current_index]
+        for student_b, text_vector_b in new_vectors:
+            sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+            student_pair = sorted((student_a, student_b))
+            score = (student_pair[0], student_pair[1], sim_score)
+            plagiarism_results.add(score)
     return plagiarism_results
 
 
